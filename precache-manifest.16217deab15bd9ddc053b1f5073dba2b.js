def055ba0ba01a8f9cb9a67126b5a3ed45144dc5
balakumaran88 <bala.mg88@gmail.com>
2020-05-23 05:48:03 +0530

    Updates

---
+++ precache-manifest.16217deab15bd9ddc053b1f5073dba2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b68f60ca206de6474dfcabc5d76bb78",
+    "url": "/mycovid3d/index.html"
+  },
+  {
+    "revision": "f83416000bae8c080899",
+    "url": "/mycovid3d/static/css/main.4d963751.chunk.css"
+  },
+  {
+    "revision": "c37a65e738830f2e630e",
+    "url": "/mycovid3d/static/js/2.cd0c539b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycovid3d/static/js/2.cd0c539b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83416000bae8c080899",
+    "url": "/mycovid3d/static/js/main.b5864acc.chunk.js"
+  },
+  {
+    "revision": "09356d33ee5077e8179d",
+    "url": "/mycovid3d/static/js/runtime-main.52951ea6.js"
+  }
+]);
